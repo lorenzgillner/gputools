fc516c6abab4632a9dcef4b57560bf535651afb4
lorenzgillner <mail@lorenzgillner.xyz>
2023-11-29 13:27:36 +0100

    Add requirements.txt and README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# gputools
+
+Collection of scripts for working with GPUs.
+
+## Requirements
+
+- python3 >= 3.6
+- gnuplot >= 5.0
+
--- wattmonplot.sh
+++ wattmonplot.sh
@@ -2,7 +2,7 @@
 if [[ -z "$DISPLAY" ]]; then
     GNUPLOT_TERMINAL="dumb"
 else
-    GNUPLOT_TERMINAL="x11"
+    GNUPLOT_TERMINAL="wxt"
 fi
 python3 util/wattmon.py "$@" | tee wattmon.log | gnuplot -p -e "set terminal $GNUPLOT_TERMINAL; set nokey; plot '-' using 2 w l"
 
